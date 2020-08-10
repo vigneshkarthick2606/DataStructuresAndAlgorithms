82bb6ce8eb7de15f9d86876529c0764271cde833
vigneshkarthick2606 <60441471+vigneshkarthick2606@users.noreply.github.com>
2020-08-10 12:09:33 +0530

    Intersection of Intervals

--- Arrays.java
+++ Arrays.java
@@ -16,6 +16,7 @@
 14. Reverse an array in groups of given size
 15. 941. Valid Mountain Array
 16. 16. 56. Merge Intervals
+17. 986. Interval List Intersections
 
 
 */
@@ -589,3 +590,45 @@ Output: [[1,4]]
     }
 	
 *-----------------------------------------------------------------------------------------------------------------------------------------------*
+/*
+17. 986. Interval List Intersections
+
+Given two lists of closed intervals, each list of intervals is pairwise disjoint and in sorted order.
+
+Return the intersection of these two interval lists.
+
+(Formally, a closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.  
+The intersection of two closed intervals is a set of real numbers that is either empty, or can be represented as a closed interval. 
+For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
+
+*/
+
+
+class Solution {
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        
+        int i = 0, j = 0;
+        List<int[]> result = new ArrayList<>();
+        
+        while(i<A.length && j<B.length){
+            
+            int lo = Math.max( A[i][0] , B[j][0]);
+            int hi = Math.min( A[i][1] , B[j][1]);
+            
+            if(lo <= hi)
+                result.add(new int[]{lo, hi});
+            
+            if(A[i][1] < B[j][1])
+                i++;
+            else
+                j++;
+
+        }
+        
+        return result.toArray(new int[result.size()][]);
+        
+    }
+}
+
+*-----------------------------------------------------------------------------------------------------------------------------------------------*
+
