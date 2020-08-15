49a8a45123e5171afa22565587b9a3a029a6f4f3
vigneshkarthick2606 <60441471+vigneshkarthick2606@users.noreply.github.com>
2020-08-15 08:57:06 +0530

    18. Product of array Itself

--- Arrays.java
+++ Arrays.java
@@ -17,7 +17,7 @@
 15. 941. Valid Mountain Array
 16. 16. 56. Merge Intervals
 17. 986. Interval List Intersections
-
+18. 238. Product of Array Except Self
 
 */
 *-----------------------------------------------------------------------------------------------------------------------------------------------*
@@ -631,4 +631,88 @@ class Solution {
 }
 
 *-----------------------------------------------------------------------------------------------------------------------------------------------*
+/*
+18. 238. Product of Array Except Self
 
+Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Example:
+
+Input:  [1,2,3,4]
+Output: [24,12,8,6]
+Constraint: It's guaranteed that the product of the elements of any prefix or suffix of the array (including the whole array) fits in a 32 bit integer.
+
+Note: Please solve it without division and in O(n).
+
+Follow up:
+Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
+
+*/
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+        int n = nums.length;
+        int[] L = new int[n];
+        int[] R = new int[n];
+        
+        L[0] = R[n-1] = 1;
+        
+        for(int i=1; i < n; i++){
+            L[i] = L[i-1] * nums[i-1];
+        }
+        
+        for(int i=n-2; i >= 0; i--){
+            R[i] = R[i+1] * nums[i+1];
+        }
+        
+         for(int i=0; i < n; i++){
+            L[i] = L[i] * R[i];
+        }       
+
+        return L;
+        
+    }
+}
+/*
+
+Given numbers [2, 3, 4, 5], regarding the third number 4, the product of array except 4 is 2*3*5 which consists of two parts: left 2*3 and right 5. The product is left*right. We can get lefts and rights:
+
+Numbers:     2    3    4     5
+Lefts:            2  2*3 2*3*4
+Rights:  3*4*5  4*5    5      
+Let’s fill the empty with 1:
+
+Numbers:     2    3    4     5
+Lefts:       1    2  2*3 2*3*4
+Rights:  3*4*5  4*5    5     1
+We can calculate lefts and rights in 2 loops. The time complexity is O(n).
+
+We store lefts in result array. If we allocate a new array for rights. The space complexity is O(n). To make it O(1), we just need to store it in a variable which is right in @lycjava3’s code.
+
+Clear code with comments:
+
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        // Calculate lefts and store in res.
+        int left = 1;
+        for (int i = 0; i < n; i++) {
+            if (i > 0)
+                left = left * nums[i - 1];
+            res[i] = left;
+        }
+        // Calculate rights and the product from the end of the array.
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i < n - 1)
+                right = right * nums[i + 1];
+            res[i] *= right;
+        }
+        return res;
+    }
+}
+
+*/
+*-----------------------------------------------------------------------------------------------------------------------------------------------*
